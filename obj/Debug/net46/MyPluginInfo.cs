a7b2c6e838bd8d5401d3ab1c40635a438db74741
quat <49324428+quattage@users.noreply.github.com>
2026-04-20 18:50:43 +0000

    trimming

--- obj/Debug/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace gttoduf
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "works.quat.gttoduf";
-        public const string PLUGIN_NAME = "gttod_uf";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
